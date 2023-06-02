51a4a1dce0e4f005c344ac5e0662600e48c4a2af
Dain <dainpark419@gmail.com>
2023-06-02 15:20:21 +1000

    Added the time interval on num wave lines

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let yOffset = 0.0;
 let waveColor;
 let numWaveLines;
+let lastChangeTime = 0;
+const changeInterval = 3000; // Change every 3 seconds
 
 function setup() {
   const canvasContainer = document.getElementById('canvas-container');
-  const canvas = createCanvas(800, 1200);
+  const canvas = createCanvas(800, 800);
   waveColor = color(0, 100, 200, 50); // Light blue with transparency
   noFill();
   
-  numWaveLines = Math.floor(random(1, 8)); // Randomly select the number of wave lines
+  numWaveLines = Math.floor(random(1, 8)); // Randomly select the initial number of wave lines
+  lastChangeTime = millis(); // Set the initial time of the last change
 }
 
 function draw() {
@@ -38,7 +41,7 @@ function draw() {
 
   // Draw foam-like dots
   stroke(255);
-  strokeWeight(5);
+  strokeWeight(1);
   const foamCount = 1;
   for (let i = 0; i < foamCount; i++) {
     const x = random(constraints[0], constraints[1]);
@@ -48,6 +51,12 @@ function draw() {
   }
 
   yOffset += 0.005;
+
+  // Check if it's time to change the number of wave lines
+  if (millis() - lastChangeTime >= changeInterval) {
+    numWaveLines = Math.floor(random(1, 11)); // Randomly select a new number of wave lines
+    lastChangeTime = millis(); // Update the time of the last change
+  }
 }
 
 function windowResized() {
